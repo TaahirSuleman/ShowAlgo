683cd6043fe873c248d37254378f74753dff2bc9
Yusuf Kathrada <kthyus001@myuct.ac.za>
2024-06-29 22:34:30 +0200

    Added basic frontend for login

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Register() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const registerUser = (e) => {
+    e.preventDefault(); // such that page does not auto-reload
+  };
+
+  return (
+    <div>
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="enter name..."
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="enter email..."
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="enter password..."
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
