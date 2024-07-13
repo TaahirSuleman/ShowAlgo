a884383741b2db0bc77c66ac4e7799e031088b67
Taahir Suleman <tai.suleman@gmail.com>
2024-07-13 14:35:54 +0200

    Parser complete with unit and integration tests passing

---
+++ backend/src/models/ast/ArrayCreation.js
@@ -0,0 +1,9 @@
+class ArrayCreation {
+  constructor(varName, values) {
+    this.type = "ArrayCreation";
+    this.varName = varName;
+    this.values = values;
+  }
+}
+
+export default ArrayCreation;
