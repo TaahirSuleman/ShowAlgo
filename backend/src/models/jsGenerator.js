99585fe07658c791001fcf10a631a0e41ea97792
Taahir Suleman <tai.suleman@gmail.com>
2024-08-19 01:39:47 +0200

    Iteration incorporated into jsGenerator

--- backend/src/models/jsGenerator.js
+++ backend/src/models/jsGenerator.js
@@ -5,6 +5,10 @@ import Transformer from "../helpers/transformer.js";
 import JavaScriptGenerator from "../helpers/JavaScriptGenerator.js"; // Corrected import
 
 class JSGenerator {
+    static writeToFile(content) {
+        fs.appendFileSync("jsOutput.txt", content + "\n");
+    }
+
     static convert(pseudocode) {
         const tokenizer = new Tokenizer(pseudocode);
         const tokens = tokenizer.tokenize();
@@ -14,6 +18,7 @@ class JSGenerator {
         const ir = transformer.transform(ast);
         const generator = new JavaScriptGenerator(ir);
         const jsCode = generator.generate().trim();
+        JSGenerator.writeToFile("Final JavaScript: " + jsCode);
         return jsCode;
     }
 }
